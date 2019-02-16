bb174498f0c2ec7cb68a6e760252d3c6aae35123
Shreyas Patil <shreyas208@gmail.com>
2019-02-15 23:16:57 -0600

    Updated template.

--- src/template_filters.py
+++ src/template_filters.py
@@ -17,3 +17,8 @@ class TemplateFilters:
 		def _filter_fmt_timestamp_full(timestamp):
 			dt = datetime.utcfromtimestamp(timestamp).replace(tzinfo=utc).astimezone(TZ)
 			return Markup(dt.strftime("%Y-%m-%d %H:%M:%S.") + dt.strftime("%f")[0:3])
+
+		@app.template_filter("fmt_datetime_local")
+		def _filter_fmt_datetime_local(timestamp):
+			dt = datetime.utcfromtimestamp(timestamp).replace(tzinfo=utc).astimezone(TZ)
+			return Markup(dt.strftime("%Y-%m-%dT%H:%M"))
