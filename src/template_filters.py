b2631be34e0bd47ecb4c38cffa9393f6daedb754
Shreyas Patil <shreyas208@gmail.com>
2019-03-06 17:55:59 -0600

    Fixed timestamp rounding issue.

--- src/template_filters.py
+++ src/template_filters.py
@@ -4,10 +4,15 @@ from jinja2 import Markup
 from pytz import utc
 
 from src.config import TZ
+from src.util import timestamp_round_up_minute
 
 
 class TemplateFilters:
 	def __init__(self, app):
+		@app.template_filter("round_timestamp")
+		def _filter_round_timestamp(timestamp):
+			return timestamp_round_up_minute(timestamp)
+
 		@app.template_filter("fmt_timestamp")
 		def _filter_fmt_timestamp(timestamp):
 			dt = datetime.utcfromtimestamp(timestamp).replace(tzinfo=utc).astimezone(TZ)
