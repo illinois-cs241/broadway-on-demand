538aa3fd0b50c557b99b3f0723c184192d181482
Shreyas Patil <shreyas208@gmail.com>
2019-03-15 16:33:11 -0500

    Bugfix.

--- src/bw_api.py
+++ src/bw_api.py
@@ -50,7 +50,7 @@ def get_grading_run_status(cid, run_id):
 	try:
 		resp = requests.get(url="%s/grading_run_status/%s/%s" % (BROADWAY_API_URL, cid, run_id), headers=HEADERS)
 		student_jobs_dict = resp.json()["data"]["student_jobs"]
-		return student_jobs_dict.values().next()
+		return list(student_jobs_dict.values())[0]
 	except requests.exceptions.RequestException as e:
 		logging.error("get_grading_run_status(cid={}, run_id={}): {}".format(cid, run_id, repr(e)))
 		return None
@@ -66,7 +66,7 @@ def get_grading_run_job_id(cid, run_id):
 	try:
 		resp = requests.get(url="%s/grading_run_status/%s/%s" % (BROADWAY_API_URL, cid, run_id), headers=HEADERS)
 		student_jobs_dict = resp.json()["data"]["student_jobs"]
-		return student_jobs_dict.keys().next()
+		return list(student_jobs_dict.keys())[0]
 	except requests.exceptions.RequestException as e:
 		logging.error("get_grading_run_job_id(cid={}, run_id={}): {}".format(cid, run_id, repr(e)))
 		return None
